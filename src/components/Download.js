7cd252624fe6092f6c3e67bcff27bebded5f48ee
Orisabiyi <orisabiyidavid@gmail.com>
2024-05-23 19:08:37 +0100

    added the content for the Download component

--- src/components/Download.js
+++ src/components/Download.js
@@ -1,3 +1,22 @@
 export default function Download() {
-  return <section className="section section-download"></section>;
+  return (
+    <section className="section section-download">
+      <div className="section__info">
+        <h2 className="header-sec-2">Download our mobile apps</h2>
+        <p>
+          Our dedicated patient engagement app and web portal allow you to
+          access information instantaneously (no tedeous form, long calls, or
+          administrative hassle) and securely
+        </p>
+        <button className="btn btn--1">Download &dArr;</button>
+      </div>
+
+      <div className="section__img">
+        <img
+          src="images/trafalgar-illustration-03.png"
+          alt="illuestration of patient making an appointment"
+        />
+      </div>
+    </section>
+  );
 }
