a8620efd687ac4d3d7b094c26d9896977b67f3b6
Orisabiyi <orisabiyidavid@gmail.com>
2024-05-23 04:07:44 +0100

    styled the header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,22 @@ export default function Header() {
   return (
     <header className="header">
       <Nav />
+
+      <div className="header__content">
+        <h1>Virtual healthcare for you</h1>
+        <p>
+          Trafalgar provides progressive, and affordable healthcare, accessible
+          on mobile and online for everyone
+        </p>
+        <button className="btn">Consult today</button>
+      </div>
+
+      <div className="header__img">
+        <img
+          src="images/header-illustration.png"
+          alt="illustration of people relating with product"
+        />
+      </div>
     </header>
   );
 }
