5928c3a35e5d6a7d6e042e2c7e02b8d34eee2aa4
Orisabiyi <orisabiyidavid@gmail.com>
2024-05-23 15:56:57 +0100

    styled the second section of the lading page

--- src/components/Provider.js
+++ src/components/Provider.js
@@ -10,6 +10,12 @@ export default function Provider() {
 
       <div className="section__info">
         <h2 className="header-sec-2">Leading healthcare providers</h2>
+        <p>
+          Trafalgar provides progressive, and affordable healthcare, accessible
+          on mobile and online for everyone. To us, it's not just work. We take
+          pride in the solutions we deliver
+        </p>
+        <button className="btn btn--1">Learn more</button>
       </div>
     </section>
   );
